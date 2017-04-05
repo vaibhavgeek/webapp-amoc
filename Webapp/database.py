f91ac23f1b3306fea2ac857f75c3b80012083c0a
abhishek981996 <abhishektiwari981996@gmail.com>
2017-04-05 16:14:51 +0530

    migrations added

--- Webapp/database.py
+++ Webapp/database.py
@@ -1,62 +1,80 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, LargeBinary
+from flask import Flask
 from sqlalchemy.sql import func
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
-Base = declarative_base()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Blog.db'
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-class Students(Base):
-    __tablename__ = 'student'
-
-    id = Column(Integer, primary_key=True)
-    rollno = Column(String(250), nullable=False)
-    password = Column(String(100), nullable=False)
-    name = Column(String(100))
-    image = Column(LargeBinary)
-    time_created = Column(DateTime(timezone=True), server_default=func.now())
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 
-class Courses(Base):
+class Students(db.Model):
+    __tablename__ = 'students'
+    id = db.Column(db.Integer, primary_key=True)
+    rollno = db.Column(db.String(250), nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100))
+    image = db.Column(db.LargeBinary)
+    time_created = db.Column(db.DateTime(
+        timezone=True), server_default=func.now())
+
+    def __init__(self, rollno, password, name, time_created):
+        self.rollno = rollno
+        self.password = password
+        self.name = name
+        self.time_created = time_created
+
+    def __repr__(self):
+        return '<Post %r>' % self.name
+
+
+class Courses(db.Model):
     __tablename__ = 'course'
-    id = Column(Integer, primary_key=True)
-    course_code = Column(String(250))
-    course_name_long = Column(String)
-    course_name_short = Column(String)
-    time_created = Column(DateTime(timezone=True), server_default=func.now())
-    year = Column(String)
-    branch = Column(String)
-    semester = Column(String)
-    syllabus_url = Column(String)
-    question_paper_url = Column(String)
-    #Owner_id = Column(Integer,ForeignKey('user.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    course_code = db.Column(db.String(250))
+    course_name_long = db.Column(db.String)
+    course_name_short = db.Column(db.String)
+    time_created = db.Column(db.DateTime(
+        timezone=True), server_default=func.now())
+    year = db.Column(db.String)
+    branch = db.Column(db.String)
+    semester = db.Column(db.String)
+
+    #Owner_id = db.Column(db.Integer,ForeignKey('user.id'))
     #user = relationship(Users)
 
 
-class TimeTable(Base):
+class TimeTable(db.Model):
     """docstring for ClassName"""
     __tablename__ = 'timetable'
 
-    id = Column(Integer, primary_key=True)
-    course_id = Column(Integer, ForeignKey('course.id'))
-    day = Column(String)
-    time = Column(String)
-    branch_code = Column(String)
-    time_created = Column(DateTime(timezone=True), server_default=func.now())
-    course_rel = relationship(Courses)
+    id = db.Column(db.Integer, primary_key=True)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'))
+    day = db.Column(db.String)
+    time = db.Column(db.String)
+    branch_code = db.Column(db.String)
+    time_created = db.Column(db.DateTime(
+        timezone=True), server_default=func.now())
+    course_rel = db.relationship('Courses')
 
 
-class Attendence(Base):
+class Attendence(db.Model):
     __tablename__ = 'attendence'
-    id = Column(Integer, primary_key=True)
-    present = Column(String)
-    date = Column(String, nullable=False)
-    timetable_id = Column(Integer, ForeignKey('timetable.id'))
-    time_created = Column(DateTime(timezone=True), server_default=func.now())
-    timetable_rel = relationship(TimeTable)
+    id = db.Column(db.Integer, primary_key=True)
+    present = db.Column(db.String)
+    date = db.Column(db.String, nullable=False)
+    timetable_id = db.Column(db.Integer, db.ForeignKey('timetable.id'))
+    time_created = db.Column(db.DateTime(
+        timezone=True), server_default=func.now())
+    timetable_rel = db.relationship('TimeTable')
 
 
 @property
@@ -87,7 +105,3 @@ def error(self):
     return {
         'invalid_rollno': self.rollno
     }
-
-
-engine = create_engine('sqlite:///Blog.db')
-Base.metadata.create_all(engine)
